e4f5a9b945056f754ceeff64ffff191a348c0f6f
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-06-15 21:38:34 +0300

    Change Move Ball

--- Ball In Square/Assets/Scripts/Player/BallController.cs
+++ Ball In Square/Assets/Scripts/Player/BallController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,17 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float _ballSpeed = 5f;
-    private Vector3 _direction; // Направление движения мяча
+    private Vector2 _direction; // Направление движения мяча
     private bool _isMoving = false; // Флаг для определения, двигается ли мяч
     [SerializeField] private ArrowController _arrowController; // Ссылка на компонент прицела
+    private Rigidbody2D _rigidbody;
 
     void Start()
     {
         _arrowController = FindObjectOfType<ArrowController>(); // Получаем ссылку на компонент прицела
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.isKinematic = true; // Включаем кинематику, чтобы мы могли управлять движением вручную
+        _rigidbody.gravityScale = 0; // Отключаем гравитацию для кинематического объекта
     }
 
     void Update()
@@ -20,21 +25,23 @@ public class BallController : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && !_isMoving)
         {
             Vector3 currentAimPosition = _arrowController.GetInitialAimPosition(); // Получаем текущую позицию прицела
-            _direction = (currentAimPosition - transform.position).normalized;
+            _direction = ((Vector2)currentAimPosition - _rigidbody.position).normalized;
             _isMoving = true;
-        }
-
-        // Перемещение мяча, если он движется
-        if (_isMoving)
-        {
-            MoveBall();
+            _rigidbody.isKinematic = false; // Выключаем кинематику, чтобы Rigidbody2D реагировал на физику
+            _rigidbody.velocity = _direction * _ballSpeed; // Устанавливаем скорость мяча
         }
     }
 
-    // Метод для перемещения мяча с постоянной скоростью
-    private void MoveBall()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        transform.position += _direction * _ballSpeed * Time.deltaTime;
+        if (other.CompareTag("Barrier"))
+        {
+            _isMoving = false;
+            _ballSpeed = 0f;
+            _rigidbody.velocity = Vector2.zero; // Обнуляем скорость мяча
+            _rigidbody.isKinematic = true; // Включаем кинематику, чтобы мяч перестал реагировать на физику
+            Debug.Log("Ball collided with barrier.");
+        }
     }
 }
 
