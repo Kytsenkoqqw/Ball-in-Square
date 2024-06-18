68dd70a80f20dcd753d1374c55f2675350757c49
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-06-18 21:54:14 +0300

    Add BallMark

---
+++ Ball In Square/Assets/Scripts/Player/BallMark.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMark : MonoBehaviour
+{
+    [SerializeField] private GameObject _trailBallPrefab; // Префаб мячика для следа
+    [SerializeField] private int _trailCount = 5; // Количество мячиков в следе
+    [SerializeField] private float _spawnInterval = 0.2f; // Интервал появления мячиков в следе
+
+    private Queue<GameObject> _trailBallsQueue = new Queue<GameObject>();
+    private float _timeSinceLastSpawn;
+
+    void Update()
+    {
+        _timeSinceLastSpawn += Time.deltaTime;
+
+        if (_timeSinceLastSpawn >= _spawnInterval)
+        {
+            _timeSinceLastSpawn = 0f;
+            SpawnTrailBall();
+        }
+    }
+
+    private void SpawnTrailBall()
+    {
+        if (_trailBallsQueue.Count >= _trailCount)
+        {
+            Destroy(_trailBallsQueue.Dequeue());
+        }
+
+        GameObject trailBall = Instantiate(_trailBallPrefab, transform.position, Quaternion.identity);
+        _trailBallsQueue.Enqueue(trailBall);
+    }
+}
